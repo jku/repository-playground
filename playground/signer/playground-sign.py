30614b0bd298f74f96fe4b6f2cd35bb4ccac832e
Jussi Kukkonen <jkukkonen@google.com>
2023-02-16 14:17:35 +0200

    Very VERY rough git wrapping for signing events
    This is quite experimental: A test to see if it could even work
    
    Plan is to allow use without git fetch/push when if user wants to:
    this is not implemented yet

--- playground/signer/playground-sign.py
+++ playground/signer/playground-sign.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from copy import deepcopy
+import random
+import string
 import subprocess
 import click
 from configparser import ConfigParser
@@ -123,34 +125,78 @@ def _get_signing_key_input() -> Key:
     return key
 
 
+def _read_settings(config_path: str) -> dict[str, str]:
+    config = ConfigParser()
+    config.read(config_path)
+    # TODO: create config if missing, ask user for values
+    if not config or "settings" not in config:
+        raise RuntimeError("Settings file not found")
+    return config["settings"]
+
+
+def _git(cmd: list[str]) -> str:
+    cmd = ["git"] + cmd
+    proc = subprocess.run(cmd, capture_output=True, check=True, text=True)
+    return proc.stdout.strip()
+
+
+def _checkout(remote: str, branch: str) -> str:
+    # TODO prepare for all kinds of mistakes:
+    # branch not existing, local branch having conflicts, current branch having changes...
+    _git(["fetch", remote, branch])
+
+    # we want a new branch that does not exist
+    rand_s = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
+    new_branch = f"{branch}-{rand_s}"
+    _git(["checkout", "--track", "-b", new_branch, f"{remote}/{branch}"])
+    return new_branch
+
+
+def _commit_push(metadata_dir: str, signing_event: str, pull_remote: str, push_remote: str, branch: str):
+    _git(["add", metadata_dir])
+    _git(["commit", "-m", f"Changes for {signing_event}"])
+
+    # push to signing event branch if we can push there
+    if pull_remote == push_remote:
+        _git(["push", push_remote, f"{branch}:{signing_event}"])
+        click.echo(f"{signing_event} updated")
+    else:
+        _git(["push", push_remote, f"{branch}:{branch}"])
+        # TODO: Unsure how to build this hard coded URL easily... maybe parsing "git config --get remote.{remote}.url"?
+        # maybe should add configuration for project name?
+        click.echo(
+            f"Create a pull request for {signing_event} on GitHub by visiting:\n"
+            f"    https://github.com/jku/test-repo-for-playground/compare/{signing_event}...{branch}?expand=1"
+        )
+
+
 @click.command()
 @click.option("-v", "--verbose", count=True, default=0)
-def cli(verbose: int):
+@click.argument("signing-event")
+def cli(verbose: int, signing_event: str):
     """Signing tool for repository-playground."""
     logging.basicConfig(level=logging.WARNING - verbose * 10)
 
-    cmd = ["git", "rev-parse", "--show-toplevel"]
-    proc = subprocess.run(cmd, stdout=subprocess.PIPE, check=True, text=True)
-    toplevel = proc.stdout.strip()
+    toplevel = _git(["rev-parse", "--show-toplevel"])
     metadata_dir = os.path.join(toplevel, "metadata")
 
     # TODO: check that this is a playground repository,
     # so we don't start creating metadata in random git repos
 
-    config = ConfigParser()
-    config.read(os.path.join(toplevel, ".playground-sign.ini"))
+    settings = _read_settings(os.path.join(toplevel, ".playground-sign.ini"))
 
-    # TODO: create config if missing, ask user for values
-    if not config or "settings" not in config:
-        raise RuntimeError("Settings file not found")
+    # TODO support not-doing-network-ops
+    # TODO: maybe do this in a temp dir to avoid messing the users git env (see error handling later)
+    # The issue with this is that it hides the git changes from user who might want to look at them
+    branch = _checkout(settings["pull-remote"], signing_event)
 
     # PyKCS11 (Yubikey support) needs the module path
     # TODO: if config is not set, complain/ask the user?
-    if "PYKCS11LIB" not in os.environ and "pykcs11lib" in config["settings"]:
-        os.environ["PYKCS11LIB"] = config["settings"]["pykcs11lib"]
+    if "PYKCS11LIB" not in os.environ and "pykcs11lib" in settings:
+        os.environ["PYKCS11LIB"] = settings["pykcs11lib"]
 
     # TODO: if config is not set, complain/ask the user?
-    user_name = config["settings"]["user-name"]
+    user_name = settings["user-name"]
 
     repo = SignerRepository(metadata_dir, user_name, _get_signing_key_input)
     if repo.state == SignerState.UNINITIALIZED:
@@ -165,8 +211,19 @@ def cli(verbose: int):
         logger.debug("Online input: %s", online_config)
 
         repo.initialize(root_config, targets_config, online_config)
+
+        try:
+            _commit_push(metadata_dir, signing_event, settings["pull-remote"], settings["push-remote"], branch)
+        except subprocess.CalledProcessError as e:
+            # TODO we may still end up with modified files here... consider doing all of this in a temp git dir?
+            click.echo(f"Error: {e.stderr}")
     else:
         click.echo("Nothing to do.")
+
+    # Return to original branch
+    _git(["checkout", "-"])
+    # TODO delete temp branch?
+
 
 if __name__ == "__main__":
     cli()
