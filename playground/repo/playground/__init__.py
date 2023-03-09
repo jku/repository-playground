61943a2fc1106087cc5d46590529dd180c99e539
Jussi Kukkonen <jkukkonen@google.com>
2023-03-09 20:40:22 +0200

    refactor signing-event: Move more code to python

--- playground/repo/playground/__init__.py
+++ playground/repo/playground/__init__.py
@@ -1,4 +1,4 @@
-from playground.status import status_cli
+from playground.status import status
 from playground.snapshot import snapshot
 from playground.bump_expiring import bump_online
 from playground.bump_expiring import bump_offline
--- playground/repo/playground/status.py
+++ playground/repo/playground/status.py
@@ -5,13 +5,22 @@
 import filecmp
 from glob import glob
 import os
+import subprocess
 import sys
+from tempfile import TemporaryDirectory
 import click
 import logging
 
 from playground._playground_repository import PlaygroundRepository
 
 logger = logging.getLogger(__name__)
+
+def _git(cmd: list[str]) -> subprocess.CompletedProcess:
+    cmd = ["git", "-c", "user.name=repository-playground", "-c", "user.email=41898282+github-actions[bot]@users.noreply.github.com"] + cmd
+    proc = subprocess.run(cmd, check=True, capture_output=True, text=True)
+    logger.debug("%s:\n%s", cmd, proc.stdout)
+    return proc
+
 
 def _find_changed_roles(known_good_dir: str, signing_event_dir: str) -> list[str]:
     # find the files that have changed or been added
@@ -37,55 +46,74 @@ def _find_changed_roles(known_good_dir: str, signing_event_dir: str) -> list[str
 
     return changed_roles
 
+
+def _role_status(repo: PlaygroundRepository, role:str) -> bool:
+    status, prev_status = repo.status(role)
+    role_is_valid = status.valid
+    sig_counts = f"{len(status.signed)}/{status.threshold}"
+    signed = status.signed
+    missing = status.missing
+
+    if prev_status:
+        role_is_valid = role_is_valid and prev_status.valid
+        sig_counts = f"{sig_counts} ({len(prev_status.signed)}/{prev_status.threshold})"
+        signed = signed | prev_status.signed
+        missing = missing | prev_status.missing
+
+    if status.invites:
+        click.echo(f"#### :x: {role}")
+        click.echo(f"{role} delegations have open invites ({', '.join(status.invites)}).")
+        click.echo(f"Invitees can accept the invitations by running `playground-sign`")
+    elif role_is_valid:
+        click.echo(f"#### :heavy_check_mark: {role}")
+        click.echo(f"{role} is verified and signed by {sig_counts} signers ({', '.join(signed)}).")
+    elif signed:
+        click.echo(f"#### :x:{role}")
+        click.echo(f"{role} is not yet verified. It is signed by {sig_counts} signers ({', '.join(signed)}).")
+    else:
+        click.echo(f"#### :x: {role}")
+        click.echo(f"{role} is unsigned and not yet verified")
+
+    if status.message:
+        click.echo(f"**Error**: {status.message}")
+    elif missing and not status.invites:
+        click.echo(f"Still missing signatures from {', '.join(missing)}")
+        click.echo(f"Signers can sign these changes by running `playground-sign`")
+
+    return role_is_valid and len(status.invites) == 0
+
+
 @click.command()
 @click.option("-v", "--verbose", count=True, default=0)
-@click.argument("known-good-dir")
-@click.argument("event-name")
-def status_cli(verbose: int, known_good_dir: str, event_name: str) -> None:
+def status(verbose: int) -> None:
     """Status markdown output tool for Repository Playground CI"""
     logging.basicConfig(level=logging.WARNING - verbose * 10)
 
-    # Find signing event metadata: we expect tool to run in git top level dir
-    signing_event_dir = "metadata"
-    good_dir = os.path.join(known_good_dir, signing_event_dir)
-    failures = 0
-
-    # Print status for each role
-    repo = PlaygroundRepository(signing_event_dir, good_dir)
-    for role in _find_changed_roles(good_dir, signing_event_dir):
-        status, prev_status = repo.status(role)
-        role_is_valid = status.valid
-        sig_counts = f"{len(status.signed)}/{status.threshold}"
-        signed = status.signed
-        missing = status.missing
-
-        if prev_status:
-            role_is_valid = role_is_valid and prev_status.valid
-            sig_counts = f"{sig_counts} ({len(prev_status.signed)}/{prev_status.threshold})"
-            signed = signed | prev_status.signed
-            missing = missing | prev_status.missing
-
-        if not role_is_valid or status.invites:
-            failures += 1
-
-        if status.invites:
-            click.echo(f"#### :x: {role}")
-            click.echo(f"{role} delegations have open invites ({', '.join(status.invites)}).")
-            click.echo(f"Invitees can accept the invitations by running `playground-sign`")
-        elif role_is_valid:
-            click.echo(f"#### :heavy_check_mark: {role}")
-            click.echo(f"{role} is verified and signed by {sig_counts} signers ({', '.join(signed)}).")
-        elif signed:
-            click.echo(f"#### :x:{role}")
-            click.echo(f"{role} is not yet verified. It is signed by {sig_counts} signers ({', '.join(signed)}).")
-        else:
-            click.echo(f"#### :x: {role}")
-            click.echo(f"{role} is unsigned and not yet verified")
-
-        if status.message:
-            click.echo(f"**Error**: {status.message}")
-        elif missing and not status.invites:
-            click.echo(f"Still missing signatures from {', '.join(missing)}")
-            click.echo(f"Signers can sign these changes by running `playground-sign`")
+    click.echo("### Current signing event state")
     
-    sys.exit(failures)
+    if not os.path.exists("metadata/root.json"):
+        click.echo("Repository does not exist yet. Create one with `playground-delegate`.")
+        sys.exit(1)
+
+    # Find the known-good commit
+    head = _git(["rev-parse", "HEAD"]).stdout.strip()
+    merge_base = _git(["merge-base", "origin/main", "HEAD"]).stdout.strip()
+    if head == merge_base:
+        click.echo("This signing event contains no changes yet")
+        sys.exit(1)
+
+    with TemporaryDirectory() as known_good_dir:
+        _git(["clone", "--quiet", ".", known_good_dir])
+        _git(["-C", known_good_dir, "checkout", "--quiet", merge_base])
+
+        good_dir = os.path.join(known_good_dir, "metadata")
+        success = True
+
+        # Compare current repository and the known good version.
+        # Print status for each role, count invalid roles
+        repo = PlaygroundRepository("metadata", good_dir)
+        for role in _find_changed_roles(good_dir, "metadata"):
+            if not _role_status(repo, role):
+                success = False
+
+    sys.exit(0 if success else 1)
