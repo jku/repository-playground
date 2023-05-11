9db4c367526ccda53c234194757fcaa5babc55c7
Fredrik Skogman <kommendorkapten@github.com>
2023-05-11 13:22:32 +0200

    Any role can now contain the x-playground-signing-period field now.
    When checking for bump of a version, the signing period is extracted
    and checked against if it is time to bump and sign the metadata.
    
    Signed-off-by: Fredrik Skogman <kommendorkapten@github.com>

---
+++ playground/repo/test/test_playground_reposiory.py
@@ -0,0 +1,43 @@
+import unittest, shutil, tempfile
+from playground._playground_repository import PlaygroundRepository
+
+class TestPlaygroundRepository(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
+    def test_non_existing_repo(self):
+        repo = PlaygroundRepository("/tmp/no_such_file")
+        self.assertRaises(ValueError, repo.open, "root")
+
+    def test_signing_expiry_days_root(self):
+        repo = PlaygroundRepository("test/test_repo1")
+        root = repo.open("root")
+
+        signing_days, expiry_days = repo.signing_expiry_period("root", root)
+        self.assertEqual(signing_days, 60)
+        self.assertEqual(expiry_days, 365)
+
+    def test_signing_expiry_days_role(self):
+        repo = PlaygroundRepository("test/test_repo2")
+
+        signing_days, expiry_days = repo.signing_expiry_period("timestamp", None)
+        self.assertEqual(signing_days, 6)
+        self.assertEqual(expiry_days, 40)
+
+    def test_default_signing_days(self):
+        repo = PlaygroundRepository("test/test_repo1")
+
+        signing_days, expiry_days = repo.signing_expiry_period("timestamp", None)
+        self.assertEqual(signing_days, 2)
+        self.assertEqual(expiry_days, 4)
+
+    # def test_bump_expires_expired(self):
+    #     repo = PlaygroundRepository("test/test_repo1")
+    #     ver = repo.bump_expiring("timestamp")
+    #     self.assertEqual(ver, 2)
+
+if __name__ == '__main__':
+    unittest.main()
